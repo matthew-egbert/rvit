7ffe793030799f74a488d78348c6e4bb532d823e
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2018-10-08 11:55:33 +1300

    Add minimal package definition, with kivy dependency.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rvit',
+      version='0.1',
+      description='Realtime interaction and visualization toolkit for scientists',
+      url='http://matthewegbert.com',
+      author='Matthew Egbert',
+      author_email='mde@matthewegbert.com',
+      license='GPLv3',
+      packages=['rvit'],
+      install_requires=['kivy'],
+      zip_safe=False)
