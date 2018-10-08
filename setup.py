e13a6e2378bbd3974f63c5ada3f4cea4be6021b8
Tommy Yu <y@metatoaster.com>
2018-10-08 20:17:48 +1300

    Packaging cleanup and completion
    - Specify the packaging of resource files.
    - Reformat the setup.py for easier reading of additional dependencies.
    - Remove the namespace __init__.py as this is unnecessary for Python 3.

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 from setuptools import setup
 
-setup(name='rvit',
-      version='0.1',
-      description='Realtime interaction and visualization toolkit for scientists',
-      url='http://matthewegbert.com',
-      author='Matthew Egbert',
-      author_email='mde@matthewegbert.com',
-      license='GPLv3',
-      packages=['rvit'],
-      install_requires=['kivy', 'cython', 'jinja2'],
-      zip_safe=False)
+classifiers = """
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+Operating System :: OS Independent
+Programming Language :: Python :: 3
+""".strip().splitlines()
+
+
+setup(
+    name='rvit',
+    version='0.1',
+    classifiers=classifiers,
+    description='Realtime interaction and visualization toolkit for scientists',
+    url='http://matthewegbert.com',
+    author='Matthew Egbert',
+    author_email='mde@matthewegbert.com',
+    # packages=find_packages('src', exclude=['ez_setup']),
+    # package_dir={'': 'src'},
+    namespace_packages=['rvit'],
+    license='GPLv3',
+    packages=['rvit'],
+    install_requires=[
+        'kivy',
+        'cython',
+        'jinja2',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
