ead11b89710113f56a9315030f313cbd36a1dbbc
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2019-04-30 23:52:13 +1200

    Add in barchart renderer.

--- rvit/core/widgets/__init__.py
+++ rvit/core/widgets/__init__.py
@@ -1,4 +1,5 @@
 from . import array_renderer
+from . import barchart_renderer
 # from .array_renderer import ArrayRenderer
 # from .point_renderer import PointRenderer
 # from .skivy_controller import SkivySlider
